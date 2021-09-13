b3afc5303a0743602213a1a803219316cf746634
Florian Pascouau <florian@pascouau.com>
2021-09-13 18:31:11 +0200

    feat(api) Added swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
 import { AppModule } from "./app/app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const globalPrefix = "api";
-    app.setGlobalPrefix(globalPrefix);
-    const port = process.env.PORT || 3333;
+    const config = new DocumentBuilder()
+        .setTitle("Sfeir talk Swagger")
+        .setDescription("The Sfeir talk swagger description")
+        .setVersion("1.0")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
+    const port = 3000;
     await app.listen(port, () => {
-        Logger.log("Listening at http://localhost:" + port + "/" + globalPrefix);
+        Logger.log("Listening at http://localhost:" + port + "/");
     });
 }
 
