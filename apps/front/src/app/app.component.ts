59f0f8dab274493ca7b042889af66b66682f6330
Florian Pascouau <florian@pascouau.com>
2021-09-13 18:23:45 +0200

    chore(prettier) Prettier all

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@sfeir-talk-swagger/api-interfaces';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Message } from "@sfeir-talk-swagger/api-interfaces";
 
 @Component({
-  selector: 'sfeir-talk-swagger-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: "sfeir-talk-swagger-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+    hello$ = this.http.get<Message>("/api/hello");
+    constructor(private http: HttpClient) {}
 }
